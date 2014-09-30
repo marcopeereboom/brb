307f9e2a1e61b57341e2c83f9b51bccbff4209eb
Marco Peereboom <marco@conformal.com>
2014-09-30 12:01:09 -0500

    doco

--- brb.go
+++ brb.go
@@ -1,3 +1,7 @@
+// Package brb (Basic Big.Rat) make using golang's big.Rats less of a pain in
+// the butt.
+// It provides a natural language interface to big.Rats instead of having to
+// deal with assembly like mnemonics.
 package brb
 
 import (
@@ -10,24 +14,36 @@ import (
 )
 
 var (
+	// ErrVariableNotFound is returned if the requested variable does not
+	// exist.
 	ErrVariableNotFound = errors.New("variable not found")
-	ErrNoResult         = errors.New("no result")
+
+	// ErrNoResult is returned if the script has not return value but was.
+	// successful.
+	// Variables may or may not have been set.
+	ErrNoResult = errors.New("no result")
 )
 
+// Interpreter is the context that is required to run expressions.
 type Interpreter struct {
 	lexer *yylexer
 	mtx   sync.Mutex
 }
 
+// New returns an Interpreter context.
 func New() *Interpreter {
 	i := Interpreter{}
 	return &i
 }
 
+// Run executes script and returns a *big.Rat if there is a result.
+// If there is no result Run returns ErrNoResult.
+// The following example returns ErrNoResult:
+//	br, err := new(Interpreter).Run("a=12.5")
 func (i *Interpreter) Run(script string, args ...interface{}) (*big.Rat, error) {
 	r := bufio.NewReader(strings.NewReader(script))
 
-	// make sure args are either *big.Rat
+	// make sure args are *big.Rat
 	for k, v := range args {
 		switch v.(type) {
 		case *big.Rat:
@@ -52,6 +68,12 @@ func (i *Interpreter) Run(script string, args ...interface{}) (*big.Rat, error) 
 	return nil, i.lexer.lastError
 }
 
+// GetVar returns a variable if it exists.
+// If the requested variable does not exist it returns ErrVariableNotFound.
+// The following example returns a *big.Rat that is set to 12/1
+//	i := New()
+//	_, err := i.Run("a=12.0")
+//	myvar, err := i.GetVar("a")
 func (i *Interpreter) GetVar(name string) (*big.Rat, error) {
 	i.mtx.Lock()
 	defer i.mtx.Unlock()
@@ -64,6 +86,7 @@ func (i *Interpreter) GetVar(name string) (*big.Rat, error) {
 	return &v, nil
 }
 
+// GetResult returns the result if it exists.
 func (i *Interpreter) GetResult(name string) (*big.Rat, error) {
 	i.mtx.Lock()
 	defer i.mtx.Unlock()
